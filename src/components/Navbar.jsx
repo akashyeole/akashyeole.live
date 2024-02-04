205d6b4863cf7e8d111658c82681eb42df5abc56
Akash <akashyeole02@gmail.com>
2024-02-04 20:14:09 +0530

    pages and components structure setup and base css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div className="nav__menu">
+        <ul className="nav__list"></ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
