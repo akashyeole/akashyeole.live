b9692ce59bfc664d64cdd58905b18f109f2694d2
Akash <akashyeole02@gmail.com>
2024-02-19 22:21:17 +0530

    added square particles in background of home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Profile from '../../assets/home.jpg'
 import { Link } from 'react-router-dom'
 import { FaArrowRight } from 'react-icons/fa'
 import './home.css'
+import Particles from 'react-tsparticles'
+import { loadSlim } from 'tsparticles-slim'
+import particlesConfig from './particles.json'
+import { loadRoundedRectShape } from "tsparticles-shape-rounded-rect";
 
 const Home = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadRoundedRectShape(engine)
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+  }, []);
+  
   return (
     <section className="home section grid">
+      <Particles
+        id="tsparticles"
+        options={particlesConfig}
+        init={particlesInit}
+        loaded={particlesLoaded}
+      />
       <img src={Profile} alt="" className="home__img" />
       <div className="home__content">
         <div className="home__data">
