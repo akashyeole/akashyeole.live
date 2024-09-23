9502dcdfdefefd71e339936440e4694241a052f7
Akash Yeole <akashyeole02@gmail.com>
2024-09-23 11:26:49 +0530

    update experience and calculate age dynamically

--- src/data.jsx
+++ src/data.jsx
@@ -36,21 +36,18 @@ export const links = [
     icon: <FaHome className='nav__icon' />,
     path: '/',
   },
-
   {
     id: 2,
     name: 'About',
     icon: <FaUser className='nav__icon' />,
     path: '/about',
   },
-
   {
     id: 3,
     name: 'Projects',
     icon: <FaFolderOpen className='nav__icon' />,
     path: '/projects',
   },
-
   {
     id: 4,
     name: 'Contact',
@@ -65,43 +62,43 @@ export const personalInfo = [
     title: 'Name : ',
     description: 'Akash Yeole',
   },
-
   {
     id: 2,
     title: 'Age : ',
-    description: '21 Years',
+    description: `${(()=>{
+      let dob = new Date('11/08/2002');
+      let month_diff = Date.now() - dob.getTime();  
+      let age_dt = new Date(month_diff);   
+      let year = age_dt.getUTCFullYear();  
+      let age = Math.abs(year - 1970);  
+      return age;
+    })()} Years`,
   },
-
   {
     id: 3,
     title: 'Nationality : ',
     description: 'India',
   },
-
   {
     id: 4,
     title: 'Freelance : ',
     description: 'Available',
   },
-
   {
     id: 5,
     title: 'Address : ',
     description: 'Nashik, Maharashtra, India',
   },
-
   {
     id: 6,
     title: 'Phone : ',
     description: '+91 775799XXXX',
   },
-
   {
     id: 7,
     title: 'Email : ',
     description: 'akashyeole02@gmail.com',
   },
-
   {
     id: 8,
     title: 'Langages : ',
@@ -115,13 +112,11 @@ export const stats = [
     no: '1+',
     title: 'Years of <br /> Experience',
   },
-
   {
     id: 2,
     no: '11+',
     title: 'Completed <br /> Projects',
   },
-
   {
     id: 3,
     no: '6+',
@@ -138,34 +133,47 @@ export const stats = [
 
 export const resume = [
   {
-    id: 1,
+    id: 5,
     category: 'experience',
     icon: <FaBriefcase />,
-    year: '2023 - PRESENT',
-    title: 'Software Engineer Intern <span> Infineon Technologies </span>',
-    desc: '<ol> <li> Worked on Digital design system by developing HTML web components(stepper, sidebar & progressbar) based on brand design using <first-color-text>Stencil JS</first-color-text> and <first-color-text>Storybook JS</first-color-text>. These DDS components  has 2000+ install on npmjs repository.</li> <li> Created exmaple webpages using <first-color-text>React JS</first-color-text> to demonstarte use of DDS components.</li> <li>Wrote <first-color-text>Python</first-color-text> script to download 1000+ files from source and upload them to a desitnation(remote FTP).</li> <li>Worked on a <first-color-text>Python</first-color-text> script that normalizes the file names and makes them SEO friendly. This script helped all the division in company to normalize more than 1000 existing files.</li> <li>Used <first-color-text>Akamai CDN tool</first-color-text> to manage URL redirection to infineon website.</li> </ol>',
+    year: '2024 - PRESENT',
+    title: 'Software Engineer <span> Infineon Technologies </span>',
+    desc: `<ol> 
+            <li> Continuing to drive the evolution of the Digital design system by incorporating new components such as Segmented controls and Chip and optmizing the functinality of existing components.</li>
+            <li> Developed exmaple webpages using <first-color-text>React JS</first-color-text> to demonstarte use of DDS components.</li> 
+            <li> Developed a <first-color-text>Python</first-color-text> automation script to streamline the file download process and securely store it in an SFTP server, significantly enhancing data management efficiency in S&M Department. </li> 
+          </ol>`,
   },  
-
   {
-    id: 2,
+    id: 4,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '2023 - 2024',
+    title: 'Software Engineer Intern <span> Infineon Technologies </span>',
+    desc: `<ol> 
+            <li> Worked on Digital design system by developing HTML web components(stepper, sidebar & progressbar) based on brand design using <first-color-text>Stencil JS</first-color-text> and <first-color-text>Storybook JS</first-color-text>. These DDS components  has 2000+ install on npmjs repository.</li>
+            <li> Pioneered a <first-color-text>Python</first-color-text> script to revolutionize file naming, ensuring SEO-friendly and standardized formats. This script facilitated the normalization of over <first-color-text>1000</first-color-text> product-related files across various divisions, streamlining the content management process.</li> 
+            <li> Employed the powerful <first-color-text>Akamai CDN</first-color-text> tool to orchestrate seamless URL redirection across the company domain, optimizing accessibility and enhancing user navigation.</li> 
+          </ol>`,
+  },  
+  {
+    id: 3,
     category: 'education',
     icon: <FaGraduationCap />,
     year: '2021 - PRESENT',
     title: 'Bachelor of Technology in Computer Science <span> Vishwakarma Institute of Information Technology, Pune </span>',
     desc: '',
   },
-
   {
-    id: 3,
+    id: 2,
     category: 'education',
     icon: <FaGraduationCap />,
     year: '2018 - 2021',
     title: 'Diploma in Computer Technology <span> K. K. Wagh Polytechnic, Nashik </span>',
     desc: '',
   },
-
   {
-    id: 4,
+    id: 1,
     category: 'education',
     icon: <FaGraduationCap />,
     year: '2018',
@@ -180,44 +188,36 @@ export const skills = [
     title: 'Javascript',
     percentage: '80',
   },
-
   {
     id: 2,
     title: 'HTML',
     percentage: '60',
   },
-
   {
     id: 3,
     title: 'CSS',
     percentage: '70',
   },
-
   {
     id: 4,
     title: 'Mongo DB',
     percentage: '70',
   },
-
   {
     id: 5,
     title: 'Python',
     percentage: '90',
   },
-  
   {
     id: 6,
     title: 'React JS',
     percentage: '80',
   },
-
-
   {
     id: 7,
     title: 'Stencil JS',
     percentage: '85',
   },
-
   {
     id: 8,
     title: 'Express JS',
@@ -231,37 +231,31 @@ export const awards = [
     title: 'Won Code Royale: Coding Competition by CSI-VIIT (Won Cash Prize)',
     link: ''
   },
-
   {
     id: 2,
     title: 'Knight on LeetCode (Top 4% in world, 1900+ Max Rating)',
     link: 'https://drive.google.com/file/d/12xLRujWs6Q5JjBO4d4KHYra0NDzAZTgH/view?usp=drive_link'
   },
-
   {
     id: 3,
     title: 'ICPC Regionalist 2023 (AIR 159)',
     link: 'https://drive.google.com/file/d/1LSk6tcKBN6KpojGZcZtpoNBcmNqiTHqN/view?usp=sharing'
   },
-
   {
     id: 4,
     title: '3 Star on Codechef (1775 Max Rating)',
     link: 'https://drive.google.com/file/d/1xMj7LWzZ2JARjM7ql5A5MLQ_Wycd2L1G/view?usp=drive_link'
   },
-
   {
     id: 5,
     title: 'Pupil on CodeForces',
     link: 'https://drive.google.com/file/d/1FXRJHSKv4zIH1D-4RwmJeG8O1SQFhi3_/view?usp=drive_link'
   },
-
   {
     id: 6,
     title: 'Top 6/104 \'7 Days of Code Challenge\' by CSI-VIIT',
     link: 'https://drive.google.com/file/d/1GmM5aGKn6MEADZ8yJlDczW912SrZHYY7/view?usp=sharing'
   },
-  
 ]
 
 export const portfolio = [
@@ -287,7 +281,6 @@ export const portfolio = [
       },
     ],
   },
-
   {
     id: 7,
     img: Project7,
@@ -310,7 +303,6 @@ export const portfolio = [
       },
     ],
   },
-  
   {
     id: 6,
     img: Project6,
@@ -343,7 +335,6 @@ export const portfolio = [
       },
     ],
   },
-
   {
     id: 5,
     img: Project5,
@@ -371,7 +362,6 @@ export const portfolio = [
       },
     ],
   },
-
   {
     id: 4,
     img: Project4,
@@ -394,7 +384,6 @@ export const portfolio = [
       },
     ],
   },
-
   {
     id: 3,
     img: Project3,
@@ -417,7 +406,6 @@ export const portfolio = [
       },
     ],
   },
-
   {
     id: 1,
     img: Project1,
@@ -435,7 +423,6 @@ export const portfolio = [
       }
     ],
   },
-
 ];
 
 export const themes = [
@@ -444,55 +431,46 @@ export const themes = [
     img: Theme1,
     color: 'hsl(252, 35%, 51%)',
   },
-
   {
     id: 2,
     img: Theme2,
     color: 'hsl(4, 93%, 54%)',
   },
-
   {
     id: 3,
     img: Theme3,
     color: 'hsl(271, 76%, 53%)',
   },
-
   {
     id: 4,
     img: Theme4,
     color: 'hsl(225, 73%, 57%)',
   },
-
   {
     id: 5,
     img: Theme5,
     color: 'hsl(43, 74%, 49%)',
   },
-
   {
     id: 6,
     img: Theme6,
     color: 'hsl(339, 81%, 66%)',
   },
-
   {
     id: 7,
     img: Theme7,
     color: 'hsl(80, 61%, 50%)',
   },
-
   {
     id: 8,
     img: Theme8,
     color: 'hsl(19, 96%, 52%)',
   },
-
   {
     id: 9,
     img: Theme9,
     color: 'hsl(88, 65%, 43%)',
   },
-
   {
     id: 10,
     img: Theme10,
